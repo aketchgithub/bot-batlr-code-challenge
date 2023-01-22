16ad00aba501250a3b2594d9533265c1b7fd2d17
@aketchgithub <vickeryace@gmaill.com>
2023-01-22 18:08:03 +0300

    Done with the first core deliverable

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import BotCollection from "./component/BotCollection";
 
+function App() {
+    return(
+        <><div className="">
+            <h1><center>BOT BATTLR.</center></h1>
+
+        </div><BotCollection /></>
+    )
+}
+
+export default App;
--- src/component/BotCollection.js
+++ src/component/BotCollection.js
@@ -1,1 +1,30 @@
-//This component should handle the fetching of data from the API and rendering the profiles of all bots.
+import React from "react";
+import Bot from "./Bot";
+class BotCollection extends React.Component {
+  state = {
+    bots: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/bots')
+    .then(response => response.json())
+    .then(data => {
+      this.setState({bots: data});
+    })
+    .catch(error =>{
+      console.error('Error fetching data', error);
+    });
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.bots.map(bot =>(
+          <Bot key = {bot.id} bot={bot}/>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default BotCollection
